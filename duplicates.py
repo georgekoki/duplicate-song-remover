9390ac5e2a75cad0f9ffae808a2b5ca4aa5ef5f3
George Kokiadis <52638095+georgekoki@users.noreply.github.com>
2022-06-03 10:55:23 +0300

    * Added Folder path and sensitivity input * Fixed files without ID3 tags throwing errors

--- duplicates.py
+++ duplicates.py
@@ -1,9 +1,19 @@
+from email.mime import base
 import Levenshtein as lev
 import os
+from os import path
+import sys
 import eyed3
 
-target_path = 'D:\Music\Genres'
-sensitivity = 0
+target_path = input("Folder path:\n")
+sensitivity = input("Sensitivity (0-10):\n")
+
+if(not path.exists(target_path) or not sensitivity.isdigit()):
+    print("Usage:\nFolder path: The folder you want to check for duplicates\n" + 
+    "Sensitivity: A value between 0 and 10 on how similar the files have to be. Higher is more.")
+    exit()
+
+sensitivity = int(sensitivity)
 
 for path, subdirs, files in os.walk(target_path):
     for name in files:
@@ -16,8 +26,17 @@ for path, subdirs, files in os.walk(target_path):
                     base_file = eyed3.load(full_path)
                     scan_file = eyed3.load(full_path_scan)
                     
-                    if(lev.distance(base_file.tag.artist, scan_file.tag.artist) <= sensitivity and
-                    lev.distance(base_file.tag.title, scan_file.tag.title) <= sensitivity ):
+                    if(hasattr(base_file, 'tag') and hasattr(scan_file, 'tag')):
+                        if(lev.distance(base_file.tag.artist, scan_file.tag.artist) <= sensitivity and
+                        lev.distance(base_file.tag.title, scan_file.tag.title) <= sensitivity ):
+                            print(base_file.tag.artist + " - " + base_file.tag.title)
+                            print("1: " + full_path + "\n" + "2: " + full_path_scan + "\n")
+                            choice = input("Delete: ")
+                            if(choice == "1"):
+                                os.remove(full_path)
+                            if(choice == "2"):
+                                os.remove(full_path_scan)
+                    else:
                         print(base_file.tag.artist + " - " + base_file.tag.title)
                         print("1: " + full_path + "\n" + "2: " + full_path_scan + "\n")
                         choice = input("Delete: ")
